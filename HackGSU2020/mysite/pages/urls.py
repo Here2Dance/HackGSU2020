738a5ac09f3578420e9e9ada71f7ad8908b12567
Here2Dance <54193586+Here2Dance@users.noreply.github.com>
2020-11-21 21:31:06 -0500

    Upated Framework
    Adds Home, FristSteps, Resources, Forum pages and connectivities

--- HackGSU2020/mysite/pages/urls.py
+++ HackGSU2020/mysite/pages/urls.py
@@ -1,7 +1,10 @@
 # pages/urls.py
 from django.urls import path
-from .views import HomePageView
+from .views import HomePageView, FirstStepsInStemView, FurtherResourcesView, ForumsView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
+    path('firststepsinstem/', FirstStepsInStemView.as_view(), name='firststepsinstem'),
+    path('furtherresources/', FurtherResourcesView.as_view(), name='furtherresources'),
+    path('forums/', ForumsView.as_view(), name='forums')
 ]
--- HackGSU2020/mysite/pages/views.py
+++ HackGSU2020/mysite/pages/views.py
@@ -4,3 +4,15 @@ from django.views.generic import TemplateView
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class ForumsView(TemplateView):
+	template_name = 'forums.html'
+
+
+class FirstStepsInStemView(TemplateView):
+	template_name = 'first_steps_in_stem.html'
+
+
+class FurtherResourcesView(TemplateView):
+	template_name = 'further_resources.html'
